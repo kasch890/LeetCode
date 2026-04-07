1191c1c0fe1d38fe6bbb2f0aba3a3042b66a871b
Kai Fernandes <kai.fernandes890@gmail.com>
2026-04-07 18:35:58 -0400

    Time: 0 ms (100%), Space: 12.2 MB (93.16%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        import math
+        return int(math.sqrt(x))
+
+        
